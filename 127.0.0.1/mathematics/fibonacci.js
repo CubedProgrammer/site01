6b18365b924501c209612f05776177f526c7aa05
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2025-04-15 23:49:56 +0000

    Fibonacci calculator.

--- 127.0.0.1/mathematics/fibonacci.js
+++ 127.0.0.1/mathematics/fibonacci.js
@@ -15,3 +15,45 @@ while(b <= 9007199254740991)
 	c = a+b
 	a = b, b = c
 }
+function onCalculate(e)
+{
+	foutput.innerHTML = null
+	try
+	{
+		index = BigInt(finput.value)
+		desired = [0n, 1n]
+		fa = 0n
+		fb = 1n
+		for(; index; index >>= 1n)
+		{
+			if(index & 1n)
+			{
+				temporary = (desired[0] + desired[1]) * fb + desired[1] * fa
+				desired[0] *= fa
+				desired[1] *= fb
+				desired[0] += desired[1]
+				desired[1] = temporary
+			}
+			fc = 2n * fa * fb
+			fa *= fa
+			fb *= fb
+			fa += fb
+			fb += fc
+		}
+		foutput.append(desired[0])
+	}
+	catch(e)
+	{
+		foutput.append(e.toString())
+		console.error(e)
+	}
+}
+function onEnter(e)
+{
+	if(e.keyCode === 13)
+	{
+		onCalculate(e)
+	}
+}
+calculateButton.addEventListener('click', onCalculate)
+document.addEventListener('keyup', onEnter)
